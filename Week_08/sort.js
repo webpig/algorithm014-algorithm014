268e3aa6fed904d78b55cf4b8b03c1138bb93ce9
webpig <11765461@qq.com>
2020-10-15 10:21:18 +0800

    feat: 新增排序相关代码

--- Week_08/sort.js
+++ Week_08/sort.js
@@ -1,69 +1,3 @@
-### 学习笔记
-
-#### 位运算
-
-常见的位运算：
-
-n & (n - 1) 除去最低位的1
-
-n & (-n) 得到最低位的1
-
-n >> 1 向右移一位，/2向下取整
-
-n & 1 可判断奇偶
-
-n ^ n 0
-
-例题：
-
-[191. 位1的个数](https://leetcode-cn.com/problems/number-of-1-bits/)
-
-代码如下：
-
-```js
-// 每次去掉最低位的1，直到n为0，只会循环1出现的次数。时间复杂度：O(1)，空间复杂度：O(1)
-var hammingWeight = function(n) {
-	let count = 0
-
-	while (n !== 0) {
-		n &= (n - 1)
-		count++
-	}
-
-	return count
-};
-```
-
-[231. 2的幂](https://leetcode-cn.com/problems/power-of-two/)
-
-代码如下：
-
-```js
-var isPowerOfTwo = function(n) {
-	return n > 0 && (n & (n - 1) === 0)
-};
-```
-
-#### 布隆过滤器
-
-用于判断是否存在，不存在是准备的，存在不一定准确。比较高效。用作中间一步缓存。
-
-#### LRU Cache
-
-最近最少使用，使用哈希表+双向链表实现。哈希表保存节点，可以实现O(1)访问任意节点，双向链表则保存了使用顺序。
-
-#### 排序
-
-比较类排序：通过比较来决定元素间的顺序，时间复杂度最低为O(nlogn)
-
-非比较类顺序：不通过比较来决定顺序，线性时间
-
-**冒泡排序**
-
-依次比较相邻的元素，每次会让最大值/最小值排到顶部
-
-```js
-// 冒泡排序
 function bubbleSort(arr) {
 	const len = arr.length
 
@@ -78,7 +12,6 @@ function bubbleSort(arr) {
 	return arr
 }
 
-// 选择排序
 function selectionSort(arr) {
 	const len = arr.length
 	let minIndex = 0
@@ -98,7 +31,6 @@ function selectionSort(arr) {
 	return arr
 }
 
-// 插入排序
 function insertionSort(arr) {
 	let preIndex = 0, curr = arr[0]
 
@@ -117,7 +49,6 @@ function insertionSort(arr) {
 	return arr
 }
 
-// 希尔排序
 function shellSort(arr) {
 	const len = arr.length
 
@@ -138,7 +69,6 @@ function shellSort(arr) {
 	return arr
 }
 
-// 堆排序
 class Heap {
 	constructor(compare) {
 		this.compare = compare
@@ -222,10 +152,10 @@ function heapSort(arr) {
 
 	return arr
 }
-```
 
-[排序算法总结](https://www.cnblogs.com/onepixel/p/7674659.html)
-
-
-
-
+const arr = [3, 1, 5, 4, 2, 5, 6, 0]
+// console.log(bubbleSort(arr))
+// console.log(selectionSort(arr))
+// console.log(insertionSort(arr))
+// console.log(shellSort(arr))
+console.log('heap', heapSort(arr)) // heap [0, 1, 2, 3, 4, 5, 5, 6]
